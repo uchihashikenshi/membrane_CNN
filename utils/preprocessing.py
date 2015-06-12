386da263c91c00ca12edf3421eebfd359da2d30d
uchihashi_k <uchihashi.kenshi@gmail.com>
2015-06-13 01:14:58 +0900

    前処理部分実装

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -10,17 +10,59 @@ import theano
 
 memCNN_home = os.getcwd() # projects/memCNNから走らせる予定
 
-class Preprocessing():
+# ✕train ◯training
+
+"""
+Annotation
+data_type: trainingとかtestとか
+data_dir: データを入れてるディレクトリの名前。data/raw/(data_type)/まではprefix
+"""
+class Preprocessing(object):
     def __init__(self):
         pass
 
-    def load_images(self, data_dir):
+    def load_images(self, data_type, data_dir):
         os.chdir("data/raw/%s" % data_dir)
         filelist = glob.glob('*.tif') # とりあえずtifのみ対応
         return filelist
 
-    def median_extract(self):
-        pass
+    def image_to_array(self, file):
+        raw_image = Image.open(file)
+        raw_matrix = np.array(list(raw_image.getdata())).reshape(1024, 1024)
+        return raw_matrix
+
+    def median_extract(self, data_type, data_dir):
+        filelist = self.load_images(data_type, data_dir)
+        if os.path.exists("%s/data/%s_dataset/median_extract_%s_dataset" % (memCNN_home, data_type, data_type)) != True:
+            os.mkdir("%s/data/%s_dataset/median_extract_%s_dataset" % (memCNN_home, data_type, data_type)) # データ置き場用意
+        # スタック中全画像からmedianを求める(medianの平均値)
+        # fixme: medianの平均でいいのか・・・？
+        N, _sum = 0, 0
+        for file in filelist:
+            raw_matrix = self.image_to_array(file)
+            median = np.median(raw_matrix)
+            _sum += median
+            N += 1
+        stack_median = _sum / N
+
+        file_num = 1
+        for file in filelist:
+            raw_matrix = self.image_to_array(file)
+            median = np.median(raw_matrix) #中央値
+            # スタックのmedianに各画像のmedianを合わせる
+            median_extract_matrix = (raw_matrix - (median - stack_median))
+            # 負の画素値を0に補正
+            # fixme: こんな処理を入れずにスマートにやりたい
+            for i in range(1024):
+                for j in range(1024):
+                    if median_extract_matrix[i][j] < 0:
+                        median_extract_matrix[i][j] = 0
+            median_extract_image = Image.fromarray(np.uint8(median_extract_matrix).reshape(1024, 1024))
+            median_extract_image.save("%s/data/%s_dataset/median_extract_%s_dataset/median_extract_image_%03d.tif" % (memCNN_home, data_type, data_type, file_num))
+            file_num += 1
+            if file_num % 10 == 0:
+                print("%s epoch ended" % file_num)
+        print("median_extract_%s_dataset is created" % data_type)
 
     def make_average_pooled_image(self, data_dir):
         filelist = self.load_images(data_dir)
@@ -31,21 +73,20 @@ class Preprocessing():
         file_num = 1
         for file in filelist:
             raw_image = Image.open(file)
-            raw_pixel_matrix = np.array(list(raw_image.getdata())).reshape(1024, 1024)
+            raw_matrix = np.array(list(raw_image.getdata())).reshape(1024, 1024)
             pool_size = (4, 4) # とりあえずベタ書き
-            pooled_pixel_matrix = []
+            pooled_matrix = []
             for i in range(int(1024 / 4)):
                 for j in range(int(1024 / 4)):
                     _sum = 0
                     for k in range(4):
                         for l in range(4):
-                            _sum += raw_pixel_matrix[4 * i + k, 4 * j + l]
+                            _sum += raw_matrix[4 * i + k, 4 * j + l]
                     pooled_pixel = round(_sum / 16)
-                    pooled_pixel_matrix.append(pooled_pixel)
-            pooled_image = Image.fromarray(np.uint8(pooled_pixel_matrix).reshape(256, 256))
-            plt.imshow(pooled_image)
+                    pooled_matrix.append(pooled_pixel)
+            pooled_image = Image.fromarray(np.uint8(pooled_matrix).reshape(256, 256))
             pooled_image.save("%s/data/raw/pooled_dataset/pooled_image_%03d.tif" % (memCNN_home, file_num))
             file_num += 1
             if file_num % 10 == 0:
                 print("%s epoch ended" % file_num)
-        print("train 100 raw tif data created")
+        print("train 100 raw tif dataset is created")
